1ad771a34bdc8297dbfa1292a33ff8be353849e4
hibbanwe <hibbanannafii23@gmail.com>
2023-04-04 15:12:22 +0700

    Hasil praktikum7

--- Pertemuan7/Hero.php
+++ Pertemuan7/Hero.php
@@ -1,13 +1,14 @@
 <?php
 class Hero{
     // Attribut / Properties pada class 
-    public $name = "Alucard";
-    public $health = 3200;
-    public $damage = 200;
+    public $name;
+    public $health;
+    public $damage;
+    public $level = 1;
 
 
     // constructor akan dijalankan secara otomatis
-    public function __construct($name, $health, $damage)
+     function __construct($name, $health, $damage)
     {
         $this->name = $name;
         $this->helath = $health;
@@ -17,14 +18,25 @@ class Hero{
     // method untuk menampilkan informasi hero
     public function getinfo(){
         echo "Nama Hero: " . $this->name;
+        echo "<br> Level: " . $this->level;
         echo "<br> HP: " . $this->health;
         echo "<br> Damage: " . $this->damage;
     }
-}
 
+
+// method untuk naik satu level
+public function levelUp(){
+    $this->level += 1;
+}
+}
 // membuat objek dari class Hero
 $hero1 = new Hero ('Alucard', 3200, 200);
 $hero2 = new Hero ('Franco', 5000, 50);
 
 // memanggil method
 $hero1->getinfo();
+
+echo "<br> <hr>";
+
+$hero1->levelUp();
+$hero1->getInfo();
